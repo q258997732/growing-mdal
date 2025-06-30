6c62959009626326b25ebefec934a429b7b529fe
apprenticebe <258997732@qq.com>
2025-06-30 10:28:52 +0800

    1557

--- src/test/java/bob/growingmdal/GrowingMdalApplicationTests.java
+++ src/test/java/bob/growingmdal/GrowingMdalApplicationTests.java
@@ -1,20 +1,23 @@
 package bob.growingmdal;
 
 import bob.growingmdal.adapter.DekaReader;
+import bob.growingmdal.service.DekaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.time.ZoneId;
 
 
 @SpringBootTest
 class GrowingMdalApplicationTests {
 
+    @Autowired
+    DekaService dekaService;
     @Test
     void contextLoads() throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
-
-
+        DekaReader reader = DekaReader.load();
     }
 }
