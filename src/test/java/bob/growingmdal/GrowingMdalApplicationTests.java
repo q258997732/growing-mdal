203d97d413d66a674df3239facd7a96185141879
apprenticebe <258997732@qq.com>
2025-06-30 14:35:17 +0800

    1557

--- src/test/java/bob/growingmdal/GrowingMdalApplicationTests.java
+++ src/test/java/bob/growingmdal/GrowingMdalApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.ZoneId;
 
@@ -19,5 +20,14 @@ class GrowingMdalApplicationTests {
     @Test
     void contextLoads() throws IOException {
         DekaReader reader = DekaReader.load();
+
+        byte[] info_buffer = new byte[512];
+        reader.LibMain(0,info_buffer);
+        System.out.println("dcrf32 version: " + DekaReader.gbk_bytes_to_string(info_buffer));
+
+        reader.LibMain(1,DekaReader.string_to_gbk_bytes("/lib/deka_T10-MX4_x64/"));
+
+
+
     }
 }
