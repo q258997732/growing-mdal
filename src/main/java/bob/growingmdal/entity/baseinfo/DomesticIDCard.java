803c0d6151e905c45dbacf967de271a51152043f
apprenticebe <258997732@qq.com>
2025-07-11 11:55:46 +0800

    1557

--- src/main/java/bob/growingmdal/entity/baseinfo/DomesticIDCard.java
+++ src/main/java/bob/growingmdal/entity/baseinfo/DomesticIDCard.java
@@ -115,6 +115,7 @@ public class DomesticIDCard implements IDCard {
                 ", \"expireStartDay\":\"" + expireStartDay + "\"" +
                 ", \"expireEndDay\":\"" + expireEndDay + "\"" +
                 ", \"reserved\":\"" + reserved + "\"" +
+                ", \"photo\":\"" + photo + "\"" +
                 "}";
     }
 
