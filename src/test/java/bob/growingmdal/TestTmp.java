09da737adef19658bb628c4386dd8bed81cc2815
apprenticebe <258997732@qq.com>
2025-07-17 12:05:05 +0800

    1557

--- src/test/java/bob/growingmdal/TestTmp.java
+++ src/test/java/bob/growingmdal/TestTmp.java
@@ -1,14 +1,6 @@
 package bob.growingmdal;
 
 import bob.growingmdal.adapter.LexmarkPrinterAdapter;
-import org.snmp4j.CommunityTarget;
-import org.snmp4j.PDU;
-import org.snmp4j.Snmp;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.event.ResponseEvent;
-import org.snmp4j.mp.SnmpConstants;
-import org.snmp4j.smi.*;
-import org.snmp4j.transport.DefaultUdpTransportMapping;
 
 import java.io.IOException;
 
@@ -17,7 +9,7 @@ import static java.lang.Thread.sleep;
 public class TestTmp {
     public static void main(String[] args) throws IOException {
         LexmarkPrinterAdapter lexmarkPrinterAdapter = new LexmarkPrinterAdapter("192.168.107.112", "public");
-        String tmp = lexmarkPrinterAdapter.getPrinterStatus();
+        String tmp = lexmarkPrinterAdapter.getPrinterErrStatus();
         System.out.println(tmp);
     }
 
