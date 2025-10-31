65cddcd4e1087f8ed54d5b771b816dd50b51e986
apprenticebe <258997732@qq.com>
2025-10-31 14:01:27 +0800

    修改adapter配置文件读取路径

--- src/main/java/bob/growingmdal/config/AdapterConfig.java
+++ src/main/java/bob/growingmdal/config/AdapterConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @PropertySource(value = {
         "classpath:adapter.properties",              // 兜底，保证本地能启动
-        "file:${CONF_DIR:.}/adapter.properties"      // 外部目录优先
+        "file:./adapter.properties"      // 外部目录优先
 }, ignoreResourceNotFound = true)   // 外部文件找不到也不报错
 @Getter
 @Setter
